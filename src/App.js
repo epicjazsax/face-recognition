ff67e73e12155572376298967cf51e6d86aebd46
Justin <121533926+epicjazsax@users.noreply.github.com>
2024-05-07 13:14:27 -0700

    Prep updated Clarifai code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,73 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
+
+const returnClarifaiJSONRequest = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = 'f9a6dca9cc4b433f9d0a132d31a5b73d';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'justinzak';       
+  const APP_ID = 'main';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions
+}
+
+
 function App() {
+  //SWITCH TO HOOKS
+  // onButtonSubmit = () => {
+  //   this.setState({imageUrl: this.state.input});
+
+  //   fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiJSONRequest(this.state.input))
+  //   .then(response => response.json())
+  //     .then(response => {
+  //       console.log('hi', response)
+  //       if (response) {
+  //         fetch('http://localhost:3000/image', {
+  //           method: 'put',
+  //           headers: {'Content-Type': 'application/json'},
+  //           body: JSON.stringify({
+  //             id: this.state.user.id
+  //           })
+  //         })
+  //           .then(response => response.json())
+  //           .then(count => {
+  //             this.setState(Object.assign(this.state.user, { entries: count }))
+  //           })
+  //       }
+  //       this.displayFaceBox(this.calculateFaceLocation(response))
+  //     })
+  //     .catch(err => console.log(err));
+  // }
   return (
     <div className="App">
       <ParticlesBg type="square" bg={true} />
